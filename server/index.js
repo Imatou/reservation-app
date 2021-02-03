7e608c19e45112c6cd80f9c8d1932f0d3a508640
Imatou <mchkimt@gmail.com>
2021-02-03 22:01:36 +0900

    HEROKU setup

--- server/index.js
+++ server/index.js
@@ -1,22 +1,32 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const config = require('./config/dev');
+const config = require('./config');
 const FakeDb = require('./fake-db');
 const productRoute = require('./routes/products');
+const path = require('path');
 
 mongoose.connect(config.DB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(
   () => {
-    const fakeDb = new FakeDb()
-    fakeDb.initDb()
+    if (process.env.NODE_ENV !== 'production') {
+      // const fakeDb = new FakeDb()
+      // fakeDb.initDb()
+    }
   }
 )
 
 const app = express();
 app.use('/apl/v1/products', productRoute);
 
+if (process.env.NODE_ENV === 'production') {
+  const appPath = path.join(__dirname, '..', 'dist', 'reservation-app');
+  app.use(express.static(appPath));
+  app.get("*", function (req, res) {
+    res.sendFile(path, resolve(appPath, 'index.hteml'));
+  });
+}
 // app.get('/product', function (req, res) {
 //   res.json({ 'sucess': true })
 // })
